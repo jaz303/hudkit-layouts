a157e81a04b0fb9462914ab82362e4a0ce73973a
Jason Frame <jason@onehackoranother.com>
2013-11-25 10:57:42 +0000

    finished first set of macros

--- demo/bundle.js
+++ demo/bundle.js
@@ -2,10 +2,18 @@
 window.hk = require('hudkit');
 window.hl = require('../');
 },{"../":2,"hudkit":3}],2:[function(require,module,exports){
+function maybeNumber(value, defaultValue) {
+    return (typeof value === 'undefined') ? defaultValue : parseFloat(value, 10);
+}
+
 function box(hk) {
     var box = new hk.Box();
     box.setBackgroundColor('#404040');
     return box;
+}
+
+function isWidget(object) {
+    return (typeof object === 'object') && (typeof object.getRoot === 'function');
 }
 
 function create(hk, thing) {
@@ -13,6 +21,8 @@ function create(hk, thing) {
         return new hk[thing]();
     } else if (typeof thing === 'function') {
         return thing(hk);
+    } else if (isWidget(thing)) {
+        return thing;
     } else {
         throw new Error("can't create widget; I don't know what to do with this: " + thing);
     }
@@ -22,24 +32,42 @@ function s2(hk, orientation, options) {
 
     options = options || {};
 
-    var layout = {
-        split: new hk.SplitPane()
-    };
+    var split = new hk.SplitPane();
+    split.setOrientation(orientation);
+    split.setSplit('split' in options ? options.split : 0.5);
 
-    layout.split.setOrientation(orientation);
-    layout.split.setSplit('split' in options ? options.split : 0.5);
+    var layout = {};
+
+    function makeWidget(widget, defaultId) {
+
+        var id = defaultId;
+
+        if (!widget) {
+            widget = box;
+        }
+
+        if (typeof widget === 'object' && !isWidget(widget)) {
+            if (widget.id) id = widget.id;
+            widget = create(hk, widget.widget);
+        } else {
+            widget = create(hk, widget);
+        }
+
+        layout[id] = widget;
+
+        return widget;
+        
+    }
 
     if (orientation === hk.SPLIT_PANE_VERTICAL) {
-        layout.left = create(hk, options.left || box);
-        layout.right = create(hk, options.right || box);
-        layout.split.setTopWidget(layout.left);
-        layout.split.setBottomWidget(layout.right);
+        split.setLeftWidget(makeWidget(options.left, 'left'));
+        split.setRightWidget(makeWidget(options.right, 'right'));
     } else {
-        layout.top = create(hk, options.top || box);
-        layout.bottom = create(hk, options.bottom || box);
-        layout.split.setTopWidget(layout.top);
-        layout.split.setBottomWidget(layout.bottom);
+        split.setTopWidget(makeWidget(options.top, 'top'));
+        split.setBottomWidget(makeWidget(options.bottom, 'bottom'));
     }
+
+    layout.split = split;
 
     hk.root.setRootWidget(layout.split);
 
@@ -59,19 +87,56 @@ exports.h2 = function(hk, options) {
 
 // horizontal split, canvas at top, console at bottom
 exports.canvasAndConsole = function(hk, options) {
+
+    options = options || {};
+
     var layout = exports.h2(hk, {
-        top: 'Canvas2D',
-        bottom: 'Console',
-        split: 0.7
+        top     : {widget: 'Canvas2D', id: 'canvas'},
+        bottom  : {widget: 'Console', id: 'console'},
+        split   : maybeNumber(options.split, 0.7)
     });
-    layout.canvas = layout.top;
-    layout.console = layout.bottom;
+    
     return layout;
+
 }
 
 // horizontal split, canvas at top, tab bar at bottom.
 // tabs can be passed via options.tabs
 exports.canvasAndTabs = function(hk, options) {
+    
+    options = options || {};
+
+    var layout = exports.h2(hk, {
+        top     : {widget: 'Canvas2D', id: 'canvas'},
+        bottom  : {widget: 'TabPane', id: 'tabPane'},
+        split   : maybeNumber(options.split, 0.7)
+    });
+
+    layout.tabs = [];
+
+    (options.tabs || []).forEach(function(tab, ix) {
+
+        var widget  = null,
+            title   = 'Tab ' + ix,
+            id      = null;
+
+        if (typeof tab === 'object' && !isWidget(tab)) {
+            var widget = create(hk, tab.widget);
+            if ('title' in tab) title = tab.title;
+            if ('id' in tab)    id = tab.id;
+        } else {
+            var widget = create(hk, tab);
+        }
+
+        layout.tabPane.addTab(title, widget);
+        layout.tabs.push(widget);
+        
+        if (id)
+            layout[id] = widget;
+
+    });
+
+    return layout;
 
 }
 
--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+function maybeNumber(value, defaultValue) {
+    return (typeof value === 'undefined') ? defaultValue : parseFloat(value, 10);
+}
+
 function box(hk) {
     var box = new hk.Box();
     box.setBackgroundColor('#404040');
     return box;
+}
+
+function isWidget(object) {
+    return (typeof object === 'object') && (typeof object.getRoot === 'function');
 }
 
 function create(hk, thing) {
@@ -9,6 +17,8 @@ function create(hk, thing) {
         return new hk[thing]();
     } else if (typeof thing === 'function') {
         return thing(hk);
+    } else if (isWidget(thing)) {
+        return thing;
     } else {
         throw new Error("can't create widget; I don't know what to do with this: " + thing);
     }
@@ -18,24 +28,42 @@ function s2(hk, orientation, options) {
 
     options = options || {};
 
-    var layout = {
-        split: new hk.SplitPane()
-    };
+    var split = new hk.SplitPane();
+    split.setOrientation(orientation);
+    split.setSplit('split' in options ? options.split : 0.5);
 
-    layout.split.setOrientation(orientation);
-    layout.split.setSplit('split' in options ? options.split : 0.5);
+    var layout = {};
+
+    function makeWidget(widget, defaultId) {
+
+        var id = defaultId;
+
+        if (!widget) {
+            widget = box;
+        }
+
+        if (typeof widget === 'object' && !isWidget(widget)) {
+            if (widget.id) id = widget.id;
+            widget = create(hk, widget.widget);
+        } else {
+            widget = create(hk, widget);
+        }
+
+        layout[id] = widget;
+
+        return widget;
+        
+    }
 
     if (orientation === hk.SPLIT_PANE_VERTICAL) {
-        layout.left = create(hk, options.left || box);
-        layout.right = create(hk, options.right || box);
-        layout.split.setTopWidget(layout.left);
-        layout.split.setBottomWidget(layout.right);
+        split.setLeftWidget(makeWidget(options.left, 'left'));
+        split.setRightWidget(makeWidget(options.right, 'right'));
     } else {
-        layout.top = create(hk, options.top || box);
-        layout.bottom = create(hk, options.bottom || box);
-        layout.split.setTopWidget(layout.top);
-        layout.split.setBottomWidget(layout.bottom);
+        split.setTopWidget(makeWidget(options.top, 'top'));
+        split.setBottomWidget(makeWidget(options.bottom, 'bottom'));
     }
+
+    layout.split = split;
 
     hk.root.setRootWidget(layout.split);
 
@@ -55,18 +83,55 @@ exports.h2 = function(hk, options) {
 
 // horizontal split, canvas at top, console at bottom
 exports.canvasAndConsole = function(hk, options) {
+
+    options = options || {};
+
     var layout = exports.h2(hk, {
-        top: 'Canvas2D',
-        bottom: 'Console',
-        split: 0.7
+        top     : {widget: 'Canvas2D', id: 'canvas'},
+        bottom  : {widget: 'Console', id: 'console'},
+        split   : maybeNumber(options.split, 0.7)
     });
-    layout.canvas = layout.top;
-    layout.console = layout.bottom;
+    
     return layout;
+
 }
 
 // horizontal split, canvas at top, tab bar at bottom.
 // tabs can be passed via options.tabs
 exports.canvasAndTabs = function(hk, options) {
+    
+    options = options || {};
+
+    var layout = exports.h2(hk, {
+        top     : {widget: 'Canvas2D', id: 'canvas'},
+        bottom  : {widget: 'TabPane', id: 'tabPane'},
+        split   : maybeNumber(options.split, 0.7)
+    });
+
+    layout.tabs = [];
+
+    (options.tabs || []).forEach(function(tab, ix) {
+
+        var widget  = null,
+            title   = 'Tab ' + ix,
+            id      = null;
+
+        if (typeof tab === 'object' && !isWidget(tab)) {
+            var widget = create(hk, tab.widget);
+            if ('title' in tab) title = tab.title;
+            if ('id' in tab)    id = tab.id;
+        } else {
+            var widget = create(hk, tab);
+        }
+
+        layout.tabPane.addTab(title, widget);
+        layout.tabs.push(widget);
+        
+        if (id)
+            layout[id] = widget;
+
+    });
+
+    return layout;
 
 }
